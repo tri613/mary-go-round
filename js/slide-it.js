5e624f70f569cb175ed1d081ddb1a2e55bf0902a
tri613 <tri613@gmail.com>
2016-07-19 18:20:04 +0800

    modify script

--- js/slide-it.js
+++ js/slide-it.js
@@ -7,42 +7,52 @@
 
 (function(){
 
-    var slideit = function(_wrapper,_slideWidth,_showNum,_options){
+    var options,wrapper,slider,slideOffset,showNum;
+
+    var slideit = function(_wrapper_selector,_slideWidth,_showNum,_options){
         _options = _options || {margin:0,slideAll:false};
         _options.margin = _options.margin || 0;
         _options.slideAll = _options.slideAll || false;
-        
-        var wrapper = document.querySelector(_wrapper),
-            slides = wrapper.querySelectorAll('.slide'),
-            slider = wrapper.querySelector('.slider'),
-            viewport = wrapper.querySelector('.slider_view'),
-            arrows = wrapper.querySelectorAll('.arrow'),
-            slideOffset;
+        options = _options;
+        showNum = _showNum;
+
+        wrapper = document.querySelector(_wrapper_selector),
+        slides = wrapper.querySelectorAll('.slide'),
+        slider = wrapper.querySelector('.slider');
+
+        var viewport = wrapper.querySelector('.slider_view'),
+            arrows = wrapper.querySelectorAll('.arrow');
 
         var init = function(){
             //set style for elements
             for(var i=0;i<slides.length;i++){
                 slides[i].style.width = _slideWidth + 'px';
-                slides[i].style.margin = "0 " + _options.margin + 'px';
+                slides[i].style.margin = "0 " + options.margin + 'px';
             }
-            slideOffset = _slideWidth+(_options.margin*2);
+            slideOffset = _slideWidth+(options.margin*2);
             viewport.style.width = slideOffset*_showNum + 'px';
             setTimeout(function(){
                 wrapper.style.height = slides[0].offsetHeight + 'px';
             },300);
 
             //add listner
+            var handler = doslide();
             for(var i=0;i<arrows.length;i++){
-                arrows[i].addEventListener('click',onSlide);
+                arrows[i].addEventListener('click',function(e){
+                    var d = this.classList.contains('pre') ? -1 : 1;
+                    handler(d);
+                });
             }
         }();
 
-        var group = 0;
-        function onSlide(e){
-            var d = this.classList.contains('pre') ? -1 : 1, 
-                offset = (_options.slideAll) ? slideOffset*_showNum*-1 : slideOffset * -1,
-                len = (_options.slideAll) ? Math.ceil(slider.offsetWidth / offset * -1) : Math.ceil(slider.offsetWidth / offset * -1) - _showNum + 1;
-            
+    };
+
+    var doslide = function(){
+        var offset = (options.slideAll) ? slideOffset*showNum*-1 : slideOffset * -1,
+            len = (options.slideAll) ? Math.ceil(slider.offsetWidth / offset * -1) : Math.ceil(slider.offsetWidth / offset * -1) - showNum + 1,
+            group = 0;
+
+        return function(d){
             group += d;
             if(group >= len){ group = 0;}
             if(group < 0){group = len-1;}
@@ -50,7 +60,7 @@
 
             slider.style.transform = 'translateX('+t+'px)';
             slider.style.webkitTransform = 'translateX('+t+'px)';
-        }
+        };
     };
 
     window.slideit = slideit;
